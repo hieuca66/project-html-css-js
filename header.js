e28ed598c446c6fa9f412f54be8ca2d3bca062a3
unknown <hieultt247@gmail.com>
2023-05-28 16:23:06 +0700

    update

---
+++ header.js
@@ -0,0 +1,63 @@
+
+document.getElementById("header").innerHTML = 
+'<a class="navbar-brand" href="./index.html">'+
+'<img src="./img/logo/logo.png" alt="" height="50px">'+
+'</a>'+
+'<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation">'+
+'<span class="navbar-toggler-icon"></span>'+
+'</button>'+
+'<div class="collapse navbar-collapse" id="main_nav">'+
+'<ul class="navbar-nav ">'+
+'<li class="nav-item active"> <a class="nav-link " href="./index.html">Home </a> </li>'+
+'<li class="nav-item"><a class="nav-link " href="#"> About </a></li>'+
+'<li class="nav-item"><a class="nav-link " href="./email.html"> Contact </a></li>'+
+
+'<li class="nav-item ">'+
+   '<a class="nav-link  " href="" "> Nước Hoa</a>'+
+  //  '<ul class="dropdown-menu fade-up nav-links">'+
+  //    '<li><a class="dropdown-item" href="#"> Submenu item 1</a></li>'+
+  //    '<li><a class="dropdown-item" href="#"> Submenu item 2 </a></li>'+
+  //    '<li><a class="dropdown-item" href="#"> Submenu item 3 </a></li>'+
+  //  '</ul>'+
+'</li>'+
+'<li class="nav-item dropdown">'+
+'<a class="nav-link " href="#" > Đồng Hồ </a>'+
+// '<ul class="dropdown-menu fade-up nav-links">'+
+//   '<li><a class="dropdown-item" href="#"> Submenu item 1</a></li>'+
+//   '<li><a class="dropdown-item" href="#"> Submenu item 2 </a></li>'+
+//   '<li><a class="dropdown-item" href="#"> Submenu item 3 </a></li>'+
+// '</ul>'+
+'</li>'+
+'<li class="nav-item dropdown">'+
+'<a class="nav-link  href="#" >Dây Chuyền </a>'+
+// '<ul class="dropdown-menu fade-up nav-links">'+
+//   '<li><a class="dropdown-item" href="#"> Submenu item 1</a></li>'+
+//   '<li><a class="dropdown-item" href="#"> Submenu item 2 </a></li>'+
+//   '<li><a class="dropdown-item" href="#"> Submenu item 3 </a></li>'+
+// '</ul>'+
+'</li>'+
+'</ul>'+
+'<ul class="navbar-nav ms-auto nav-links">'+
+'<form class="d-flex">'+
+'<input id="search" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">'+
+'<button class="btn"><i class="fa-solid fa-magnifying-glass fa-1x"></i> </button>'+
+'</form>'+
+'<li class="nav-item"><a class="nav-link " href="#"> <i class="fa-solid fa-user"></i> </a></li>'+
+
+'<li class="nav-item  container-shop ">'+
+'<header>'+
+  
+    '<div class="shopping " id="gh">'+
+      '<i class="fa-solid fa-cart-shopping"></i>'+
+
+      '<span class="quantity total-soluong">0</span>'+
+  '</div>'+
+  
+  
+
+'</header>'+
+
+   
+'</li>'+
+'</ul>'+
+'</div>'
---
+++ js/data.js
@@ -0,0 +1,47 @@
+const category=[
+    { cat_id:'watch', cat_name:'Đồng hồ' },
+    { cat_id:'perfume', cat_name:'Nước hoa' },
+    { cat_id:'necklace', cat_name:'Dây chuyền' },
+];
+const product=[
+    { id:1, name:'TISSOT LE LOCLE POWERMATIC 80 T006.407.22.033.00', 
+    price:22220002, img:'https://cdn3.dhht.vn/wp-content/uploads/2018/05/95_T006.407.22.033.00-1-699x699.jpg',cat_id:'watch1' 
+    },
+    { id:2, name:'CASIO WORLD TIME AE-1200WHD-1AVDF', 
+    price:24220002, img:'https://cdn3.dhht.vn/wp-content/uploads/2017/07/AE-1200WHD-1AVDF-699x699.jpg',cat_id:'watch2' 
+    },
+    { id:3, name:'CITIZEN TSUYOSA NJ0154-80H', 
+    price:3200000, img:'https://cdn3.dhht.vn/wp-content/uploads/2021/12/NJ0154-80H-699x699.jpg',
+    cat_id:'watch3' 
+    },
+    { id:4, name:'ORIENT OPEN HEART RA-AR0001S10B', 
+    price:22220002, img:'https://cdn3.dhht.vn/wp-content/uploads/2019/04/36_RA-AR0001S10B-699x699.jpg',cat_id:'watch4',
+    
+    },
+    { id:5, name:'Dolce&Gabbana K EDT', 
+    price:24220002, img:'https://www.thegioinuochoa.com.vn/image/get?file=item_product%2Fthumb1500x500%2F202305%2F230503_195840_740136.png',cat_id:'perfume1' 
+    },
+    { id:6, name:'Versace Eros Parfum', 
+    price:3200000, img:'https://www.thegioinuochoa.com.vn/image/get?file=item_product%2Fthumb1500x500%2F202305%2F230503_200446_141312.png',
+    cat_id:'perfume2' 
+    },
+    { id:7, name:'Paco Rabanne Fame EDP', 
+    price:22220002, img:'https://www.thegioinuochoa.com.vn/image/get?file=item_product%2Fthumb1500x500%2F202305%2F230503_200330_359360.png',cat_id:'perfume3' 
+    },
+    { id:8, name:'HUGO BOSS INFINITE EDP2', 
+    price:24220002, img:'https://www.thegioinuochoa.com.vn/image/get?file=item_product%2Fthumb1500x500%2F202305%2F230503_200259_871335.png',cat_id:'perfume4' 
+    },
+    { id:9, name:'Golden Bottle Jade 18k', 
+    price:24220002, img:'https://ngoctham.com/wp-content/uploads/2023/03/bo-trang-suc-ngoc-trai-vang-18k-R162-ntj-01-688x774.jpg',cat_id:'perfume4' 
+    },
+    { id:10, name:'Gold necklace 18k 699', 
+    price:24220002, img:'https://ngoctham.com/wp-content/uploads/2023/05/day-chuyen-vang-18k-dvdrtvv0000p510-ntj-01-688x774.jpg',cat_id:'perfume4' 
+    },
+    { id:11, name:'Gold necklace 18k 793', 
+    price:24220002, img:'https://ngoctham.com/wp-content/uploads/2022/12/day-chuyen-vang-18k-dvdotvv0000q793-ntj-01-688x774.jpg',cat_id:'perfume4' 
+    },
+    { id:12, name:'Swarovski Colored Stone 18k', 
+    price:24220002, img:'https://ngoctham.com/wp-content/uploads/2022/11/day-chuyen-da-mau-swarovski-vang-18k-dwdohvv0000o898-ntj-01-688x774.jpg',cat_id:'perfume4' 
+    },
+
+]
